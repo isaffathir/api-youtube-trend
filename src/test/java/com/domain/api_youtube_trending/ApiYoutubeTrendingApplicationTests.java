44a59212f8fd4e9978899dde31660c1b922c33c0
isaffathir <isaffathir@outlook.com>
2021-09-07 22:11:38 +0700

    final

---
+++ src/test/java/com/domain/api_youtube_trending/ApiYoutubeTrendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.api_youtube_trending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiYoutubeTrendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
